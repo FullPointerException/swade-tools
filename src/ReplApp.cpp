50bccd4efd1b59388957cde24b56c7b8b7932768
FullPointerException <FullPointerXception@gmail.com>
2021-06-25 14:45:06 -0500

    Added generators tool menu

--- src/ReplApp.cpp
+++ src/ReplApp.cpp
@@ -12,9 +12,10 @@ ReplApp::ReplApp(std::istream& input, std::ostream& output)
 
 int ReplApp::run()
 {
-    _out << "Welcome to SWADE Tools: " << std::endl;
+    _out << "Welcome to SWADE Tools:" << std::endl;
 
     bool running = true;
+    bool isMain = true;
     while(running)
     {
         _out << "> ";
@@ -23,20 +24,63 @@ int ReplApp::run()
 
         std::string parsedInput = InputSanitizer::sanitize(rawInput);
 
-        if( (parsedInput == std::string("q")) || (parsedInput == std::string("quit")) )
+        if(isMain)
         {
-            running = false;
-        }
-        else if( (parsedInput == std::string("h")) || (parsedInput == std::string("help")) )
-        {
-            _out << "Available commands:" << std::endl;
-            _out << "\th, help\t\t\tDisplay list of available commands" << std::endl;
-            _out << "\tq, quit\t\t\tExit the application" << std::endl;
+            if( (parsedInput == std::string("q")) || (parsedInput == std::string("quit")) )
+            {
+                running = false;
+            }
+            else if( (parsedInput == std::string("h")) || (parsedInput == std::string("help")) )
+            {
+                _out << "Available commands:" << std::endl;
+                _out << "\tgenerators\t\tGenerate various things from tables" << std::endl;
+                _out << "\th, help\t\t\tDisplay list of available commands" << std::endl;
+                _out << "\tq, quit\t\t\tExit the application" << std::endl;
+            }
+            else if( (parsedInput == std::string("generators")))
+            {
+                _out << "Select Generator:" << std::endl;
+                isMain = false;
+            }
+            else
+            {
+                _out << "Invalid command '" << rawInput << "'." << std::endl;
+                _out << "Try \"help\" for a list of commands." << std::endl;
+            }
         }
         else
         {
-            _out << "Invalid command '" << rawInput << "'." << std::endl;
-            _out << "Try \"help\" for a list of commands." << std::endl;
+            if( (parsedInput == std::string("q")) || (parsedInput == std::string("quit")) )
+            {
+                _out << "SWADE Tools:" << std::endl;
+                isMain = true;
+            }
+            else if( (parsedInput == std::string("h")) || (parsedInput == std::string("help")) )
+            {
+                _out << "Available commands:" << std::endl;
+                _out << "\ttrade goods" << std::endl;
+                _out << "\th, help\t\t\tDisplay list of available commands" << std::endl;
+                _out << "\tq, quit\t\t\tReturn to the main menu" << std::endl;
+                _out << "\texit\t\t\tExit the application" << std::endl;
+            }
+            else if(parsedInput == std::string("trade goods"))
+            {
+                _out << "Trade Goods" << std::endl;
+            }
+            else if(parsedInput == std::string("exit"))
+            {
+                running = false;
+            }
+            else if( (parsedInput == std::string("generators")))
+            {
+                _out << "Select Generator:" << std::endl;
+                isMain = false;
+            }
+            else
+            {
+                _out << "Invalid command '" << rawInput << "'." << std::endl;
+                _out << "Try \"help\" for a list of commands." << std::endl;
+            }
         }
     }
 
